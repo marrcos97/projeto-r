9d77d8d4e4996288140956ea01aa010b5f7b35fd
marrcos.97 <marrcos.97@gmail.com>
2020-03-09 20:48:19 -0300

    primeira aula de r

--- aula01.R
+++ aula01.R
@@ -9,3 +9,24 @@ b <- c(1,2,3,4)
 a >= b
 a < b
 
+
+cpf <- 45711980866
+cpf
+
+fraude <- substr(cpf,9,9)
+fraude
+
+
+paste("Marcos","gomes")
+paste0("Marcos ","gomes", sep = '-' )
+
+a <- "Data science"
+b <- substr(a,6,12)
+b
+
+x <- c(1,2,3,4,15)
+mean(x)
+sd(x)
+median(x)
+max(x)
+min(x)
