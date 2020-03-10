edd90d066032acaa65955a6d529ffe53a63e7836
marrcos.97 <marrcos.97@gmail.com>
2020-03-09 22:52:01 -0300

    primeira aula de r

--- aula01.R
+++ aula01.R
@@ -48,3 +48,80 @@ cadastro$filhos
 prestacao <- c(350,280,415)
 cadastro$prestacao <- prestacao
 cadastro
+
+percentualComprometimentoRenda <- c(prestacao / salario * 100)
+cadastro$percentualComprometimentoRenda <- percentualComprometimentoRenda
+cadastro
+
+mean(percentualComprometimentoRenda)
+median
+
+#removendo a quarta coluna
+cadastro_1 <-cadastro[,-4]
+cadastro_1
+
+#mostrando o nome das colunas
+names(cadastro)
+names(cadastro_1)
+
+
+levels(salario)
+
+#não funciona
+levels(cadastro$salario)
+#funciona
+levels(cadastro$nome)
+
+library(readxl)
+Ordenar <- read_excel("C:/marcos/arquivos da aula/Ordenar.xlsx")
+View(Ordenar)
+
+attach(Ordenar)
+
+names(Ordenar)
+dim(Ordenar)
+
+Ordenar[order(Ordenar$id, decreasing = T),]
+Ordenar[order(Ordenar$id),]
+
+remover_ordenar <- unique(Ordenar)
+remover_ordenar
+
+remover_duplicados <- remover_ordenar[!duplicated(remover_ordenar$id,fromLast = TRUE),]
+remover_duplicados <- remover_ordenar[!duplicated(remover_ordenar$id,fromLast = FALSE),]
+remover_duplicados
+
+library(readxl)
+Compras <- read_excel("C:/marcos/arquivos da aula/Compras.xlsx")
+View(Compras)
+
+head(Compras)
+
+#install.packages("dplyr")
+library(dplyr)
+
+agregar <- summarise(group_by(Compras,id),
+                     Valor_Compra = mean(Valor_Compra),
+                     DT_Compra = max(DT_Compra),
+                     contagem = n())
+
+agregar
+
+#install.packages(c("rfm","lubridate"))
+library(rfm)
+library(lubridate)
+
+data_atual <- lubridate::as_date("2020-03-09", tz="UTC-3")
+agregar$data_atual <- data_atual
+
+View(agregar)
+
+attach(agregar)
+DT_Compra_1 <- lubridate::as_date(DT_Compra)
+agregar$DT_Compra_1 <- DT_Compra_1
+agregar
+
+rfm_resultado <- rfm_table_order(agregar,id, DT_Compra_1,Valor_Compra,data_atual)
+rfm_resultado
+
+View(rfm_resultado$rfm)
